04fdd7be35b19aee06ce9e6e9eab2ab6c5c1cf46
Martin Stoffel <martin.adam.stoffel@gmail.com>
2021-07-03 10:08:37 +0100

    parallelise alphapeel within chr

--- create_spec.R
+++ create_spec.R
@@ -1,15 +1,18 @@
 # make a alpha peel spec file
 library(glue)
 
-create_spec <- function(spec, nsnps, genos, ped, out) {
+create_spec <- function(spec, nsnps, genos, ped, out, startsnp, endsnp) {
         
         out <- glue("
          nSnp           , {nsnps}
          InputFilePath  , {genos}
          pedigree       , {ped}
          OutputFilePath , {out}
-         nCycles        , 20
+         nCycles        , 10
          runType        , multi
+         startsnp       , {startsnp}
+         endsnp         , {endsnp}
+         
              ")
         
         writeLines(out, spec)
