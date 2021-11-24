eccee5bff138715c41dc29d21cb419c60782202d
Martin Stoffel <martin.adam.stoffel@gmail.com>
2021-11-24 11:30:10 +0000

    slim

--- analyse_slim.R
+++ analyse_slim.R
@@ -1,3 +1,5 @@
+library(tidyverse)
+source("theme_simple.R")
 muts <- read_delim("slim_sim/sims/muts/mutperind_100.txt")
 
 test <- muts %>% 
@@ -10,16 +12,24 @@ test <- muts %>%
         summarise(mean(freq), min(freq), max(freq))
 test
 
-test <- muts %>% 
+mut_classes <- muts %>% 
         group_by(mut_id, pos, s, originG) %>% 
         tally() %>% 
         mutate(freq = n/400) %>% 
         ungroup() %>% 
-        mutate(s_class = cut(s, breaks = c(0, -0.001, -0.01, c(seq(-0.1, -1.2, by = -0.1)))))
+        mutate(s = ifelse(s < -1, -1, s)) %>% 
+        mutate(s_class = cut(s, breaks = c(0, -0.001, -0.01, c(seq(-0.1, -1.1, by = -0.1)))),
+               s_class = as.character(s_class)) %>% 
+        rowwise() %>% 
+        mutate(max_class = str_split_fixed(s_class, ",", 2)[1],
+               max_class = as.numeric(str_remove(max_class, "\\("))) %>% 
+        ungroup() %>% 
+        mutate(max_class = ifelse(max_class == -1.1, -1, max_class))
+        
 
-ggplot(test, aes(s_class, freq)) +
-        geom_jitter(width = 0.2, alpha = 0.3) +
-        scale_y_sqrt()
+ggplot(mut_classes, aes(s_class, freq)) +
+        geom_jitter(width = 0.2, alpha = 0.3) 
+
 
 # expected
 s = 0.3
@@ -29,6 +39,7 @@ O_k_hom <- (1-s) * 500
 n <- 5952
 O_k_nonhom <- n-O_k_hom
 E_k_nonhom <- n-E_k_hom
+
 # 5952
 chisq <- ((O_k_hom - E_k_hom)^2)/E_k_hom + ((O_k_nonhom - E_k_nonhom)^2)/E_k_nonhom
 chi_p <- pchisq(chisq, df = 1, lower.tail = FALSE)
@@ -52,7 +63,24 @@ pars <- expand_grid(freq = seq(0.01, 1, 0.01), s = c(0, -0.001, -0.01, c(seq(-0.
 pars$p <- map2(pars$freq,pars$s, get_p) %>% 
         unlist()
 
-pars %>% 
+pars_thres <- pars %>% 
         group_by(s) %>% 
         filter(p <= 0.05) %>% 
-        filter(freq == min(freq))
+        filter(freq == min(freq)) %>% 
+        ungroup()
+pars_thres[8, 2] <- -0.3
+
+df <- mut_classes %>% 
+        rename(mut_freq = freq) %>% 
+        left_join(pars_thres, by = c("max_class" = "s"))
+df_sum <- df %>% 
+                group_by(s_class) %>% 
+                summarise(threshold = mean(freq))
+
+ggplot(df, aes(s_class, mut_freq)) +
+        geom_jitter(width = 0.2, alpha = 0.3) +
+        geom_point(aes(s_class, threshold), data = df_sum, color = "blue", size = 2) +
+        theme_simple()
+        
+        
+
